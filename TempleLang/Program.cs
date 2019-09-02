172e09f7088c17d59e94ca1054bb1278fc3b788d
blenderfreaky <blendcrafterxiv@gmail.com>
2019-09-02 19:13:42 +0200

    Add project files.

---
+++ TempleLang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempleLang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
