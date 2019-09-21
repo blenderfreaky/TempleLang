93d3a137c86eb6d4a85991035a6727e38c42cc0c
blenderfreaky <blendcrafterxiv@gmail.com>
2019-09-22 00:42:40 +0200

    Add ILexer Interface

---
+++ TempleLang.Lexer.Abstractions/ILexer.cs
@@ -0,0 +1,12 @@
+﻿namespace TempleLang.Lexer.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface ILexer<out TToken, out TTokenType>
+        where TToken : IToken<TTokenType>
+    {
+        IEnumerable<TToken> Tokenize(TextReader text);
+    }
+}
