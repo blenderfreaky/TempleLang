0ca7b27995c7a92956c956c7a9dc6e262d9e4c72
blenderfreaky <blendcrafterxiv@gmail.com>
2019-09-22 23:53:39 +0200

    Start adding lexer implementation

--- TempleLang.Lexer/Token.cs
+++ TempleLang.Lexer/Token.cs
@@ -10,5 +10,15 @@
         public int TokenIndex { get; }
         public int FirstCharIndex { get; }
         public int LastCharIndex { get; }
+
+        public Token(string text, TokenType tokenType, IFile file, int tokenIndex, int firstCharIndex, int lastCharIndex)
+        {
+            Text = text;
+            TokenType = tokenType;
+            File = file;
+            TokenIndex = tokenIndex;
+            FirstCharIndex = firstCharIndex;
+            LastCharIndex = lastCharIndex;
+        }
     }
 }
--- TempleLang.Lexer/TokenType.cs
+++ TempleLang.Lexer/TokenType.cs
@@ -2,5 +2,38 @@
 {
     public enum TokenType
     {
+        StatementDelimiter,
+        Declarator,
+        TypeSetter,
+        KeywordReturn,
+        KeywordIf,
+        KeywordElse,
+        KeywordWhile,
+        KeywordFor,
+        Not,
+        Assign,
+        LeftExpressionDelimiter,
+        RightExpressionDelimiter,
+        LeftCodeDelimiter,
+        RightCodeDelimiter,
+        LeftEnumerationDelimiter,
+        RightEnumerationDelimiter,
+        Add,
+        Multiply,
+        BitwiseXor,
+        ComparisonGreaterThan,
+        Or,
+        BitwiseOr,
+        Subtract,
+        Divide,
+        ComparisonLessThan,
+        BinaryNegate,
+        And,
+        BitwiseAnd,
+        ComparisonGreaterOrEqualThan,
+        ComparisonLessOrEqualThan,
+        StaticAccessor,
+        ComparisonNotEquals,
+        Identifier
     }
 }
